1d8e747497f5060878ed0c932598ddb0c3d67415
Kevin <knphillips01@gmail.com>
2019-09-17 12:03:23 -0500

    added github api key and env var

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem'; 
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
-
 
 const Users = ({ users, loading }) => {
   
@@ -19,7 +18,7 @@ const Users = ({ users, loading }) => {
     }  
   }  
 
-  Users.propTypes ={
+  Users.propTypes = {
     users: PropTypes.array.isRequired,
     loading: PropTypes.bool.isRequired
   }
